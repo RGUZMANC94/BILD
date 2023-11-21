b7ab3d987ea0f1f1cad0bcc74c6fa3b0a936a67a
Manuel Medrano <151546303+mdmedranom@users.noreply.github.com>
2023-11-21 11:00:39 -0500

    oportunities-all implemented

---
+++ pages/oportunities-all.jsx
@@ -0,0 +1,67 @@
+import OportunitiesAll from "../components/oportunitiesAll";
+import Button from "../components/button";
+import { openPopUp } from "../redux/popUpOportunity";
+import styles from "../styles/Oportunities-All.module.css";
+import { useDispatch } from "react-redux";
+import { useState } from 'react';
+
+
+
+const OportunitiesAllFilter = () => {
+    const [showBar, setShowBar] = useState(false);
+
+    const [showSection, setShowSection] = useState("all");
+
+    const toggleShowBar = () => {
+        setShowBar(!showBar);
+    };
+
+
+  return (
+    
+        <section className={styles["main"]}>
+
+            <div className={styles["top-content-buttonsBar"]}>
+                <div className={styles["top-content-container"]}>
+                    <button
+                    className={styles["top-content-buttons"]}
+                    onClick={() =>  setShowSection("all")}
+                    >
+                    TODAS
+                    </button>
+                    <div className={styles["top-content-bar"]}></div>
+                </div>
+                <div className={styles["top-content-container"]}>
+                    <button
+                    className={styles["top-content-buttons"]}
+                    onClick={() =>  setShowSection("pending")}
+                    >
+                    PENDIENTES
+                    </button>
+                    <div className={styles["top-content-bar"]}></div>
+                </div>
+                <div className={styles["top-content-container"]}>
+                    <button
+                    className={styles["top-content-buttons"]}
+                    onClick={() =>  setShowSection("closed")}
+                    >
+                    CERRADAS
+                    </button>
+                    <div className={styles["top-content-bar"]}></div>
+                </div>
+            </div>
+
+           
+
+                {showSection === "all" && <OportunitiesAll />}
+                
+           
+            
+        </section>
+        
+        
+  );
+};
+
+
+export default OportunitiesAllFilter;
