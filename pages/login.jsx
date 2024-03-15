22700b01d8b63acab99d37dee04ec0e68242e3a9
Crish777 <cristhianelden@gmail.com>
2024-03-15 12:04:48 -0500

    updates

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,16 +1,7 @@
 import styles from '../styles/Login.module.css';
 import LogInComponent from '../components/login';
-import { useEffect } from 'react';
-import { getSessionToken } from '../utils/getSessionToken';
-import { useRouter } from 'next/router';
 
 const LogIn = () => {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   if (getSessionToken()) {
-  //     router.push('/');
-  //   }
-  // }, []);
   return (
     <section className={styles['main-container']}>
       <div className={styles.background}>
