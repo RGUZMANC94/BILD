e267d2d17baaf2ec4b79348053bf6d0acef57507
ManuelPaperplane <151546303+mdmedranom@users.noreply.github.com>
2024-07-30 12:00:56 -0500

    Profile and cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,18 @@ module.exports = {
       backdropBlur: {
         bild: '10px',
       },
+      fontWeight: {
+        thin: '100',
+        hairline: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      }
     },
   },
   plugins: [],
